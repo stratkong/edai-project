32ab2ea4cefeeeb2092d8ca8b7256b616e72c750
Onkar Kapuskari <on.kaps18@gmail.com>
2023-05-06 12:39:05 +0530

    addition,subtraction,multiplication

--- src/animweb/helpers/Matrix.ts
+++ src/animweb/helpers/Matrix.ts
@@ -24,11 +24,64 @@ export default class Matrix {
     }
 
     addition (matrix: Matrix) {
+        //@ts-ignore
+        const numRows1: number = math.size(matrix.matrix)[0];
+        //@ts-ignore
+        const numCols1: number = math.size(matrix.matrix)[1];
+        //@ts-ignore
+        const numRows2: number = math.size(this.matrix)[0];
+        //@ts-ignore
+        const numCols2: number = math.size(this.matrix)[1];
 
-
+        if (numRows1 !== numRows2 || numCols1 !== numCols2) {
+            throw new Error("Matrix dimensions must match");
+        }
+        
         const addMatrix = math.add(matrix.matrix,this.matrix);
-        let finalMatrix = new Matrix();
+        var finalMatrix = new Matrix();
         finalMatrix.matrix = addMatrix;
+        return finalMatrix;
+
+    }
+
+    subtraction (matrix: Matrix) {
+        //@ts-ignore
+        const numRows1: number = math.size(matrix.matrix)[0];
+        //@ts-ignore
+        const numCols1: number = math.size(matrix.matrix)[1];
+        //@ts-ignore
+        const numRows2: number = math.size(this.matrix)[0];
+        //@ts-ignore
+        const numCols2: number = math.size(this.matrix)[1];
+
+        if (numRows1 !== numRows2 || numCols1 !== numCols2) {
+            throw new Error("Matrix dimensions must match");
+        }
+        
+        const subtMatrix = math.subtract(matrix.matrix,this.matrix);
+        var finalMatrix = new Matrix();
+        finalMatrix.matrix = subtMatrix;
+        return finalMatrix;
+
+    }
+
+    multiplication (matrix: Matrix) {
+        //@ts-ignore
+        const numRows1: number = math.size(matrix.matrix)[0];
+        //@ts-ignore
+        const numCols1: number = math.size(matrix.matrix)[1];
+        //@ts-ignore
+        const numRows2: number = math.size(this.matrix)[0];
+        //@ts-ignore
+        const numCols2: number = math.size(this.matrix)[1];
+
+        if (numCols1 !== numRows1) {
+            throw new Error("Matrix multiplication is not possible.");
+        }
+        
+        const multMatrix = math.multiply(matrix.matrix,this.matrix);
+        var finalMatrix = new Matrix();
+        finalMatrix.matrix = multMatrix;
         return finalMatrix;
 
     }
